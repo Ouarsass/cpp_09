51c953b1c389aeb2dea7e98676ad837cbab2604b
mustapha ouarsass <hzan1996@gmail.com>
2023-03-18 04:14:58 +0100

    final 00

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -3,28 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mouarsas <mouarsas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mustapha <mustapha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 20:26:51 by mouarsas          #+#    #+#             */
-/*   Updated: 2023/03/17 20:10:40 by mouarsas         ###   ########.fr       */
+/*   Updated: 2023/03/18 04:09:42 by mustapha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <string>
+#include <map>
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
 #include <iostream>
 
-class BitcoinExchange
-{
-private:
-	std::string _date;
-	float _btc;
-	int _err;
-public:
-    BitcoinExchange();
-    ~BitcoinExchange();
-	BitcoinExchange & operator=(const BitcoinExchange &obg);
+class BitcoinExchange {
+	
+	public:
+		BitcoinExchange(const std::string& bitcoinPricesFile);
+		BitcoinExchange(const BitcoinExchange& other);
+		~BitcoinExchange();
+		BitcoinExchange& operator=(const BitcoinExchange& other);
+		double getExchangeRate(const std::string& date) const;
+		
+	private:
+		std::map<std::string, double>* m_prices;
+		void copy(const BitcoinExchange& other);
+		void clear();
+
 };
 
 #endif
