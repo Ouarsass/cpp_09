d1e66f491dee571b5926b38fce46f7046860b0b2
mustapha ouarsass <hzan1996@gmail.com>
2023-03-21 22:46:46 +0000

    subject pdf

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,26 +6,22 @@
 /*   By: mouarsas <mouarsas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 20:27:06 by mouarsas          #+#    #+#             */
-/*   Updated: 2023/03/21 17:51:25 by mouarsas         ###   ########.fr       */
+/*   Updated: 2023/03/21 19:36:50 by mouarsas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
 RPN::RPN(){}
- 
 RPN::RPN(RPN const &obg)
 {*this = obg;}
-
 RPN::~RPN(){}
-
 RPN &RPN::operator=(RPN const &obg)
 {
     if (this != &obg)
         this->stocker = obg.stocker;
     return (*this);
 }
-
 void RPN::dataBase(std::string dataBase)
 {
     for (int i = 0; i < (int)dataBase.length(); i++)
@@ -37,11 +33,10 @@ void RPN::dataBase(std::string dataBase)
             int nbr = dataBase[i] - '0';
             stocker.push(nbr);
         }
-        else if ((dataBase[i] == '*' || dataBase[i] == '/' || dataBase[i] == '+' || dataBase[i] == '-') && stocker.size() >= 2)
+        else if (stocker.size() >= 2 && (dataBase[i] == '*' || dataBase[i] == '/' || dataBase[i] == '+' || dataBase[i] == '-'))
         {
             int second = stocker.top();
             // std::cout <<second<<" ---------here\n";
-            
             stocker.pop();
             int first = stocker.top();
             // std::cout <<first<<" ---------here===> \n";
@@ -67,17 +62,11 @@ void RPN::dataBase(std::string dataBase)
             std::cerr << "Error" << std::endl;
             exit(1);
         }
-    
     }
     if(stocker.size() != 1)
-    {
-        std::cout << "Error1" << std::endl;
-        exit(1);
-    }
+        {std::cerr << "Error: in syntax" << std::endl;exit(1);}
     std::cout << stocker.top() << std::endl;
 }
-    // Après avoir parcouru tous les éléments, on vérifie qu'il ne reste qu'une seule opérande dans la pile.
-    // Si ce n'est pas le cas, on affiche une erreur. Sinon, on affiche le résultat final.
 //     if (stocker.size() != 1)
 //         {std::cerr << "Error: in syntax" << std::endl;return;}
 //     std::cout << stocker.top() << std::endl;
