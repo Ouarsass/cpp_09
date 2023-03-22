1c09e1a158afb8c17ffd4d4fbdd646cb69d049d1
mustapha ouarsass <hzan1996@gmail.com>
2023-03-22 04:27:59 +0000

    ex02 updated

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -3,10 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mouarsas <mouarsas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mustapha <mustapha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 18:31:02 by mouarsas          #+#    #+#             */
-/*   Updated: 2023/03/17 18:31:03 by mouarsas         ###   ########.fr       */
+/*   Updated: 2023/03/22 04:25:57 by mustapha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <string>
+#include <deque>
+#include <vector>
+#include <chrono>/////////
+class PmergeMe
+{
+	private:
+		std::deque<int> _deque;////// the deque container
+		std::vector<int> _vector;
+	public:
+		PmergeMe(int ac, char **av);
+		PmergeMe(const PmergeMe &copy);
+		~PmergeMe();
+		PmergeMe & operator=(const PmergeMe &assign);
+		
+		int check(std::string str);
+		template <typename T>
+		void print(T &container);
+		template <typename T>
+		void sort(T &container);
+		template <typename T>
+		void marge(T &container, T &left, T &right);
+		template <typename T>
+		void insert_sort(T &container);
+		
+};
+
+#endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,10 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mouarsas <mouarsas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mustapha <mustapha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 00:29:55 by mouarsas          #+#    #+#             */
-/*   Updated: 2023/03/18 00:29:56 by mouarsas         ###   ########.fr       */
+/*   Updated: 2023/03/22 04:22:03 by mustapha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+int main(int ac, char **av)
+{
+    if(ac < 2)
+    {
+        std::cout << "Error" << std::endl;
+        return 0;
+    }
+    PmergeMe pmergeme(ac, av);
+    return 0;
+}
